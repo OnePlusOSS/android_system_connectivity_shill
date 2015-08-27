0a1cb70d70ae297a4cba3b5787f09cacbef815a7
Samuel Tan <samueltan@google.com>
2015-08-27 15:48:26 -0700

    shill: Add libshill-net target
    Add libshill-net target to the shill Android.mk file.
    
    BUG: 22956441
    TEST: cd into shill folder, run 'mm' and ensure that libshill-net is built
          successfully.
    
    Change-Id: I757c08de68a65458830c71206a6ac7f2e135b66f

--- Android.mk
+++ Android.mk
@@ -19,6 +19,10 @@ LOCAL_PATH := $(call my-dir)
 
 shill_cpp_extension := .cc
 
+shill_c_includes := \
+    system/connectivity/ \
+    external/gtest/include/
+
 shill_shared_libraries := \
     libchrome \
     libchromeos \
@@ -56,3 +60,40 @@ ifeq ($(shill_use_wired_8021x)), yes)
 else
 shill_c_flags += -DDISABLE_WIRED_8021X
 endif
+
+# libshill-net
+# ========================================================
+include $(CLEAR_VARS)
+LOCAL_CPP_EXTENSION := $(shill_cpp_extension)
+LOCAL_MODULE := libshill-net
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES := $(libshill_net_sources)
+LOCAL_CFLAGS := $(shill_c_flags)
+LOCAL_CPPFLAGS := $(shill_cpp_flags) -Wno-sign-promo
+LOCAL_RTTI_FLAG := -frtti
+LOCAL_SHARED_LIBRARIES := $(shill_shared_libraries)
+LOCAL_C_INCLUDES := $(shill_c_includes)
+LOCAL_SRC_FILES := \
+    net/attribute_list.cc \
+    net/byte_string.cc \
+    net/control_netlink_attribute.cc \
+    net/event_history.cc \
+    net/generic_netlink_message.cc \
+    net/io_handler_factory.cc \
+    net/io_handler_factory_container.cc \
+    net/io_input_handler.cc \
+    net/io_ready_handler.cc \
+    net/ip_address.cc \
+    net/netlink_attribute.cc \
+    net/netlink_manager.cc \
+    net/netlink_message.cc \
+    net/netlink_packet.cc \
+    net/netlink_socket.cc \
+    net/nl80211_attribute.cc \
+    net/nl80211_message.cc \
+    net/rtnl_handler.cc \
+    net/rtnl_listener.cc \
+    net/rtnl_message.cc \
+    net/shill_time.cc \
+    net/sockets.cc
+include $(BUILD_SHARED_LIBRARY)
