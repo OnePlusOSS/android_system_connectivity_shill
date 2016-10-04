b914f7179da88711195f013bceff3072dfd4865d
waynelee <Wayne.Lee@oneplus.tw>
2016-10-04 16:01:24 +0800

    [15811N]:[connectivity][nfc][patch] Nfc disable test-rpc-proxy due to compile error for NFC porting
    Plan: None
    Type: USER
    Test: Normal Test
    
    Change-Id: I43f6e832be24c3cf56cb335f2b32417746834b1a
    (cherry picked from commit d0aafd2742c768927a3153ad047633b86d275add)

--- Android.mk
+++ Android.mk
@@ -647,23 +647,23 @@ include $(BUILD_EXECUTABLE)
 
 # test-rpc-proxy
 # ========================================================
-include $(CLEAR_VARS)
-LOCAL_MODULE := shill-test-rpc-proxy
-LOCAL_MODULE_TAGS := eng tests
-LOCAL_CPPFLAGS := $(shill_cpp_flags)
-LOCAL_SHARED_LIBRARIES := \
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := shill-test-rpc-proxy
+#LOCAL_MODULE_TAGS := eng tests
+#LOCAL_CPPFLAGS := $(shill_cpp_flags)
+#LOCAL_SHARED_LIBRARIES := \
     $(shill_shared_libraries) \
     libshill-client \
     libbrillo-dbus \
     libchrome-dbus \
     libxmlrpc++
-LOCAL_C_INCLUDES := \
+#LOCAL_C_INCLUDES := \
     $(shill_c_includes) \
     external/cros/system_api/dbus \
     external/xmlrpcpp/src
-$(eval $(shill_cpp_common))
-LOCAL_SRC_FILES := $(call all-cpp-files-under,test-rpc-proxy)
-include $(BUILD_EXECUTABLE)
+#$(eval $(shill_cpp_common))
+#LOCAL_SRC_FILES := $(call all-cpp-files-under,test-rpc-proxy)
+#include $(BUILD_EXECUTABLE)
 
 endif # SHILL_USE_BINDER
 
